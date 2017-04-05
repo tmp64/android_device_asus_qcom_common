ff6ccb174af7315df3d7dfbe48c0252f63df0ee6
Jaekyun Seok <jaekyun@google.com>
2017-04-05 11:23:26 +0900

    msm8916-common: Enforce RRO on framework-res
    Overlays only for framework-res will be converted into RROs.
    
    Other overlays can't be converted due to some known issues on app RRO.
    
    Bug: 36231603
    Change-Id: Idca30fdbbcc990fd124de16a06a112a346612a61
    (cherry picked from commit db1b6058200fc5255f519cf6609fca7619d6743e)
    Signed-off-by: Sakil_Mondal <mondalsakil@gmail.com>

--- msm8916.mk
+++ msm8916.mk
@@ -18,6 +18,9 @@
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
 
+PRODUCT_ENFORCE_RRO_TARGETS := \
+    framework-res
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
